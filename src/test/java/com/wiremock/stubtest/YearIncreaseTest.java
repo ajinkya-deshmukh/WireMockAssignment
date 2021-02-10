c4c288669100a1510ebff6b213a189c7594d1c4f
Ajinkya Deshmukh <ajinkya.deshmukh04@gmail.com>
2021-02-10 17:54:44 +0530

    Moved the test to src/test.

---
+++ src/test/java/com/wiremock/stubtest/YearIncreaseTest.java
@@ -0,0 +1,57 @@
+package com.wiremock.stubtest;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.models.IPLPlayers;
+import com.wiremock.assignment.GetPlayersService;
+import com.wiremock.assignment.CalculateIncrease;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import com.services.stub.SetupStub;
+
+public class YearIncreaseTest {
+
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule();
+	
+	@Before
+	public void setup() {
+		wireMockRule.start();
+		SetupStub.getStub();
+		System.out.println("Inside The Before Class");
+	}
+	
+	@Test
+	public void getYearOnYearIncrease() {
+		
+		GetPlayersService gpd = new GetPlayersService();
+		
+		Response response = gpd.getPlayers();
+		
+		int statusCode = response.getStatusCode();
+		System.out.println(statusCode);
+		
+		JsonPath jsonPathEvaluator = response.jsonPath();
+		
+		List<IPLPlayers> playersList = jsonPathEvaluator.getList("ipl", IPLPlayers.class);
+		
+		CalculateIncrease.calculateIncrease(playersList);
+	
+	}
+	
+	@After
+	public void tearDown() {
+		wireMockRule.stop();
+	}
+}
